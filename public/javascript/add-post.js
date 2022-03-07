bad3a11cb8dfeec8a763e0f4a83653fa00e3b8a0
danielle-gan <danielle.gan@uconn.edu>
2022-03-06 20:47:28 -0500

    fixed adding post js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,26 +4,22 @@ async function newFormHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value;
   const post_url = document.querySelector('textarea[name="post-content"]').value.trim();
 
-  if (post_url) {
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        post_url
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_url
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
   }
-
 }
-
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
